fb807df79ea6d6355a2ba7125a577f00b0fecd8d
Kaija Dunklin <kaykay_s@yahoo.com>
2020-07-13 17:05:13 -0400

    1

---
+++ app.js
@@ -0,0 +1,22 @@
+var animal ={
+    type: "bird",
+    color: "red",
+    tail: "short"
+}
+var person ={
+    gender: "female",
+    ethinity: "american",
+    hair: "long"
+}
+var object ={
+    size: "small",
+    shape: "cube",
+    name: "clock"
+}
+console.log(animal.color);
+console.log(person.hair);
+console.log(object.name);
+
+function funTest(object,person){
+    console.log(object,person);
+}
